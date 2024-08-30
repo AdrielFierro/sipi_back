3d8391ef18c4fec3f087a131e2397e18b3f6e959
AdrielFierro <adri.fierro@outlook.com>
2024-08-30 16:44:31 -0300

    url fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(json());
 
 
 app.use(cors({
-    origin: 'https://sipi.onrender.com/',
+    origin: 'https://sipi.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
